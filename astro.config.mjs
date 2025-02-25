98f4e163690582967d3b4eb8052bba309bdeca97
hub-ry <rhubbart@purdue.edu>
2025-02-25 16:26:14 -0500

    Build to docs with .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  outDir: 'dist', // Add this line
   integrations: [mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
